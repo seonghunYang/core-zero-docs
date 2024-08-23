5d0ea3410ba27a98c2488a41eae215737693d8bd
SeonghunYang <didtjdgns852@naver.com>
2024-08-23 05:03:41 +0000

    chore: Update theme.config.tsx to enable dark mode and adjust primary hue and saturation

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+  darkMode: true,
+  primaryHue: 120,
+  primarySaturation: 40,
+};
 
-export default config
+
+export default config;
